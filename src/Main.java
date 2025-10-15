0c58204f5844a31dcae323207ae7365770256e7f
lykorislove <apathyxdxdq@gmail.com>
2025-10-16 01:07:06 +0200

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Vistula");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello Vistula " + i);
+        }
     }
 }
